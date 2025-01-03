1b612b296797bd459238112152f5630b64e0df0b
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-03 19:42:25 -0300

    implementing IHostedService

---
+++ poc.hosted.service/Jobs/MyHostedJob.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace poc.hosted.service.Jobs;
+
+internal class MyHostedJob : IHostedService
+{
+    private readonly ILogger<MyHostedJob> _logger;
+    private Timer _timer;
+
+    public MyHostedJob(ILogger<MyHostedJob> logger) =>
+        _logger = logger;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Service started.");
+
+        _timer = new Timer(
+            async state => await DoWorkAsync(cancellationToken), 
+            null, 
+            TimeSpan.Zero, 
+            TimeSpan.FromSeconds(5) // execute each 5 seconds
+        );
+
+        return Task.CompletedTask;
+    }
+
+    private async Task DoWorkAsync(CancellationToken ct)
+    {
+        _logger.LogInformation($"Task started at {DateTime.Now}");
+
+        try
+        {
+            await Task.Delay(1000); // Simulating async work
+            _logger.LogInformation($"Task concluided at {DateTime.Now}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error in executing taks.");
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        _timer?.Dispose();
+        return Task.CompletedTask;
+    }
+}
---
+++ poc.hosted.service/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using poc.hosted.service.Jobs;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.AddHostedService<MyHostedJob>();
+    })
+    .Build();
+
+await host.RunAsync();
