29abf64ae194a6a8a1fa4804fe084870f770a4ce
Wellington M. Fernandes <wellington_fernands@yahoo.com.br>
2025-01-03 19:49:50 -0300

    implementing backgroundservice

---
+++ poc.background.service/Jobs/MyBackgroundServiceJob.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace poc.background.service.Jobs;
+
+internal class MyBackgroundServiceJob : BackgroundService
+{
+    private readonly ILogger<MyBackgroundServiceJob> _logger;
+
+    public MyBackgroundServiceJob(ILogger<MyBackgroundServiceJob> logger) =>
+        _logger = logger;
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        _logger.LogInformation("Service started in background.");
+
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation($"Task started at {DateTime.Now}");
+            
+            await Task.Delay(5000, stoppingToken); // Simulating some work
+        }
+
+        _logger.LogInformation("Background service was cancelled.");
+    }
+}
---
+++ poc.background.service/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using poc.background.service.Jobs;
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddHostedService<MyBackgroundServiceJob>();
+
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+
+
+
+using var host = builder.Build();
+await host.RunAsync();
